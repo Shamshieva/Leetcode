f87d348fb9f11452e68b45cf132b9ffe219d9739
burulai <burulaiurbaeva7@gmail.com>
2023-07-22 23:30:13 +0600

    canPlaceFlowers

--- src/Solution.java
+++ src/Solution.java
@@ -3,37 +3,45 @@ import java.util.Arrays;
 public class Solution {
 
     public static void main(String[] args) {
-
-        System.out.println(mergeAlternately("ab", "cd"));
-    }
-
-    public static String mergeAlternately(String word1, String word2) {
-        if (word1.length() > word2.length()) return null;
-        StringBuilder mergerString = new StringBuilder();
-        for (int i = 0; i < word1.length(); i++) {
-            mergerString.append(word1.charAt(i)).append(word2.charAt(i));
-        }
-        mergerString.append(word2.charAt(word1.length()));
-        return mergerString.toString();
+//        System.out.println(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
+//        System.out.println(canPlaceFlowers(new int[]{0,0,1,0,1}, 1));
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1,0,0}, 2));
+//        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1,0,0,0,0}, 4));
+//        System.out.println(canPlaceFlowers(new int[]{1}, 0));
+//        System.out.println(canPlaceFlowers(new int[]{0}, 1));
     }
 
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int frequent, fir;
-        for (int i = 0; i < flowerbed.length; ) {
-            frequent = 0;
-            fir = flowerbed[i];
-            while (fir == flowerbed[i]){
-                frequent++;
-                if (n*2+1 <= frequent) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
                     return true;
                 }
-                i++;
-            }
-            if (i == flowerbed.length-1){
-                break;
             }
         }
         return false;
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder mergerString = new StringBuilder();
+        if (word1.length() <= word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                mergerString.append(word1.charAt(i)).append(word2.charAt(i));
+            }
+            mergerString.append(word2.substring(word1.length()));
+        }
+        if (word1.length() > word2.length()) {
+            for (int i = 0; i < word2.length(); i++) {
+                mergerString.append(word1.charAt(i)).append(word2.charAt(i));
+            }
+            mergerString.append(word1.substring(word2.length()));
+        }
+        return mergerString.toString();
     }
 
 //    public static int[] topKFrequent(int[] nums, int k) {
